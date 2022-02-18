d3c6d99a31b826d6c94558636610afd0f3fc210e
ashleylovette <ashleylovette15@gmail.com>
2022-02-18 15:52:27 -0600

    exercise-1.4

---
+++ part-1-exercises/exercise-1.4.rb
@@ -0,0 +1,9 @@
+name_1 = "Rusty"
+name_2 = "Mac"
+name_3 = "Kubo"
+
+def name_of_my_pets (name_1, name_2, name_3) 
+    puts "I have three pets! Their names are #{name_1}, #{name_2}, and #{name_3}!"
+end
+
+name_of_my_pets(name_1, name_2, name_3) 
