6c368040ac755fe1c7f565400c3ebf28299878b3
ashleylovette <ashleylovette15@gmail.com>
2022-02-18 15:39:43 -0600

    exercise-1.2

---
+++ part-1-exercises/exercise-1.2.rb
@@ -0,0 +1,12 @@
+MY_NAME = "Ashley"
+
+def say_hello_to
+    puts "Hello, #{MY_NAME}"
+end
+
+
+say_hello_to 
+
+
+# The directions say to pass the constant variable as an argument. 
+# However, when I did this it gave me an error: "formal argument cannot be a constant"
