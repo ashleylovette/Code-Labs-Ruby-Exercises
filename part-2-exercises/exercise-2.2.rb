8220b10fd51c30fa93233297ca3c5547707513a0
ashleylovette <ashleylovette15@gmail.com>
2022-02-24 13:27:19 -0600

    exercise-2.2

--- part-2-exercises/exercise-2.2.rb
+++ part-2-exercises/exercise-2.2.rb
@@ -1,13 +1,12 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # puts array[0]
-
 # puts array[9]
 
 array.unshift(0)
-
 array << 11
 
+# FOR LOOP
 # for a in array do
 #     b = a*2
 #     if (b%2 == 0)
@@ -17,4 +16,13 @@ array << 11
 #     end
 # end
 
-puts array.select { |num| puts num*2 }
+
+# SELECT METHOD
+puts array.select { |num| 
+    b = num*2 
+    if (b%2 ==0)
+        puts "#{b} is an even number."
+    else
+        puts "#{b} is an odd number"
+    end
+}
