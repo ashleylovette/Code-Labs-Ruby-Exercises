0ae85320ed5596c2a01179c3c30b62589eb2a318
ashleylovette <ashleylovette15@gmail.com>
2022-02-24 12:48:19 -0600

    exercise-2.6

---
+++ part-2-exercises/exercise-2.5.rb
@@ -0,0 +1,14 @@
+users = [
+    {
+        name: "John Doe",
+        age: 43
+    },
+    {
+        name: "Amy Singer",
+        age: 53
+    },
+    {
+        name: "Jimmy Lendricks",
+        age: 23
+    }
+]
